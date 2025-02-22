25df06623fbcfbcf898b1dd4b7e9734f384c33bb
MaivMinh <maivanminh.se@gmail.com>
2025-02-22 23:17:42 +0700

    Update filter and search in user management component

--- src/components/UserManagement.jsx
+++ src/components/UserManagement.jsx
@@ -1,8 +1,18 @@
 import React, { useContext, useEffect } from "react";
 import userServices from "../services/userService.js";
-import { Spin, Input, Table } from "antd";
+import {
+  Spin,
+  Input,
+  Table,
+  Button,
+  Form,
+  Dropdown,
+  Checkbox,
+  message,
+} from "antd";
 import { AppContext } from "../context/AppContext.jsx";
 import { useNavigate } from "react-router-dom";
+import { DownOutlined, FilterOutlined } from "@ant-design/icons";
 const { Search } = Input;
 
 const UserManagement = () => {
@@ -10,12 +20,28 @@ const UserManagement = () => {
   const [loading, setLoading] = React.useState(true);
   const [error, setError] = React.useState(null);
   const [filteredUsers, setFilteredUsers] = React.useState([]);
-  const {isAuthenticated} = useContext(AppContext);
+  const { isAuthenticated } = useContext(AppContext);
   const navigate = useNavigate();
+  const [messageApi, contextHolder] = message.useMessage();
+  const [form] = Form.useForm();
 
   if (!isAuthenticated) {
     navigate("/login");
   }
+
+  const warning = () => {
+    messageApi.open({
+      type: "warning",
+      content: "There is no user matching the search criteria",
+    });
+  };
+
+  const success = (value) => {
+    messageApi.open({
+      type: "success",
+      content: `There are ${value} users matching the search criteria`,
+    });
+  };
 
   const displayedUsers = React.useMemo(() => {
     return filteredUsers.length > 0 ? filteredUsers : users;
@@ -45,7 +71,11 @@ const UserManagement = () => {
         user.login.username.toLowerCase().includes(content.toLowerCase())
       );
     });
-    console.log(filtered);
+    if (filtered.length === 0) {
+      warning();
+    } else if (filtered.length > 0) {
+      success(filtered.length);
+    }
     setFilteredUsers(filtered);
   };
 
@@ -53,6 +83,32 @@ const UserManagement = () => {
     if (e.target.value === "") {
       setFilteredUsers(users);
     }
+  };
+
+  const handleFilter = (values) => {
+    console.log(values);
+
+    const filtered = users.filter((user) => {
+      return (
+        (!values.status ||
+          values.status.length === 0 ||
+          values.status.includes(user.status)) &&
+        (!values.role ||
+          values.role.length === 0 ||
+          values.role.includes(user.role))
+      );
+    });
+    if (filtered.length === 0) {
+      warning();
+    } else if (filtered.length > 0) {
+      success(filtered.length);
+    }
+    setFilteredUsers(filtered);
+  };
+
+  const handleClearFilter = () => {
+    form.resetFields();
+    setFilteredUsers(users);
   };
 
   const columns = [
@@ -84,15 +140,49 @@ const UserManagement = () => {
     },
   ];
 
+  const items = [
+    {
+      key: "1",
+      label: (
+        <Form
+          onFinish={handleFilter}
+          form={form}
+          layout="vertical"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <Form.Item name="status" label="Status">
+            <Checkbox.Group>
+              <Checkbox value="active">Active</Checkbox>
+              <Checkbox value="inactive">Inactive</Checkbox>
+            </Checkbox.Group>
+          </Form.Item>
+          <Form.Item name="role" label="Role">
+            <Checkbox.Group>
+              <Checkbox value="admin">Admin</Checkbox>
+              <Checkbox value="user">User</Checkbox>
+            </Checkbox.Group>
+          </Form.Item>
+          <Button type="primary" htmlType="submit">
+            Filter
+          </Button>
+          <Button style={{ marginLeft: 8 }} onClick={handleClearFilter}>
+            Clear Filter
+          </Button>
+        </Form>
+      ),
+    },
+  ];
+
   return (
     <div className="w-full h-full">
+      {contextHolder}
       {loading ? (
         <div className="text-center mt-10">
           <Spin size="large" />
         </div>
       ) : (
         <div>
-          <div className="w-full grid grid-cols-10">
+          <div className="w-full grid grid-cols-10 gap-x-3">
             <p className="col-span-9">
               <Search
                 placeholder="Tìm kiếm theo tên người dùng hoặc email..."
@@ -103,11 +193,27 @@ const UserManagement = () => {
                 size="large"
                 onSearch={onSearch}
                 onChange={handleChange}
+                variant="underlined"
               />
             </p>
-            <p className="col-span-1 bg-red-200"></p>
+            <p className="col-span-1">
+              <Dropdown
+                menu={{
+                  items,
+                }}
+                placement="bottomLeft"
+                trigger={["click"]}
+              >
+                <Button size="large" style={{ width: "100%", height: "100%" }}>
+                  <span>Lọc</span>
+                  <p>
+                    <FilterOutlined />
+                  </p>
+                </Button>
+              </Dropdown>
+            </p>
           </div>
-          <div>
+          <div className="mt-5">
             <Table
               dataSource={displayedUsers}
               columns={columns}
